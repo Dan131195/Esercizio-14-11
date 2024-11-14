e739f9d05ad380f4eccce783d7efbf45369d514a
Dan131195 <daniele.franceschini1395@gmail.com>
2024-11-14 18:22:09 +0100

    Esercizio

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -19,33 +19,48 @@ for (let i = 0; i < 90; i++) {
 
 function extraction() {
   btnNumber.addEventListener("click", function () {
-    let extractNumber = Math.floor(Math.random() * 90) + 1;
-    extractNumbers.push(extractNumber);
-    console.log(extractNumbers);
-    let showNumber = document.getElementById("extractNumber");
-    showNumber.innerText = extractNumber;
-    const extract = document.querySelectorAll("#container div");
-    const extractTable = document.querySelectorAll("#table div");
-    for (let i = 0; i < extract.length; i++) {
-      if (
-        extract[i].innerText == extractNumber) {
-        extract[i].classList.add("extract");
-      }
-    }
-    for (let i = 0; i < extractTable.length; i++) {
-        if (
-          extractTable[i].innerText == extractNumber) {
-          extractTable[i].classList.add("extract");
-        }
-      }
+    extract();
   });
 }
+
+function extract() {
+  let extractNumber = Math.floor(Math.random() * 90) + 1;
+  if (extractNumbers.lenght == 90) {
+    alert('Hai esaurito tutti i numeri')
+    btnNumber.setAttribute('disabled', 'true')
+    return;
+  }
+  else if (extractNumbers.includes(extractNumber)) {
+    extract();
+  } else {
+    extractNumbers.push(extractNumber);
+    extracted(extractNumber);
+  }
+  let showNumber = document.getElementById("extractNumber");
+  showNumber.innerText = extractNumber;
+  
+}
+function extracted(divNumber) {
+    const extract = document.querySelectorAll("#container div");
+    const extractTable = document.querySelectorAll("#table div");
+    extract.forEach((element) => {
+        if (element.innerText == divNumber) {
+            element.classList.add('extract')
+        }
+    });
+    extractTable.forEach((element) => {
+        if (element.innerText == divNumber) {
+            element.classList.add('extract')
+        }
+    })
+}
+
 
 function table() {
   for (let i = 0; i < 24; i++) {
     const numberTable = document.createElement("div");
     numberTable.innerText = Math.floor(Math.random() * 90) + 1;
-    numberTable.classList.add('table')
+    numberTable.classList.add("table");
     tables.appendChild(numberTable);
   }
 }
